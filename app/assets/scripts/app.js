5f61e84e345bc847b3a599ca6e4896fdbce296dd
Anna Brikodili <brikodili.troyan@gmail.com>
2019-03-18 20:28:27 +0200

    refactoring

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -5,18 +5,18 @@ let ceil = document.getElementsByClassName("game-item"),
     player = "x",
     stepCount = 0,
     winCombinations = [
-        "123",
-        "147",
-        "159",
-        "258",
-        "369",
-        "357",
-        "456",
-        "789"
+    [1, 2, 3],
+    [1, 4, 7],
+    [1, 5, 9],
+    [2, 5, 8],
+    [3, 6, 9],
+    [3, 5, 7],
+    [4, 5, 6],
+    [7, 8, 9]
     ],
     playerSequence = {
-        x: "",
-        o: "",
+        x: [],
+        o: [],
     };
 
 table.addEventListener("click", currentStep);
@@ -31,16 +31,16 @@ function currentStep(event) {
         playerSequence[player] += num;
         currentTarget.classList.add(player);
 
-        if (playerSequence[player].length > 2 && isWin(playerSequence[player])) {
+        if (isWin(playerSequence[player])) {
             table.removeEventListener("click", currentStep);
             return (message.innerText = "Победил игрок " + player);
+        } else {
+            changePlayer();
+            stepCount++;
+            stepCount === 9
+                ? (message.innerText = "Ничья")
+                : (message.innerText = "Ходит игрок " + player);
         }
-
-        changePlayer();
-        stepCount++;
-        stepCount === 9
-            ? (message.innerText = "Ничья")
-            : (message.innerText = "Ходит игрок " + player);
     }
 }
 
@@ -51,19 +51,24 @@ function changePlayer() {
 reset.addEventListener("click", function() {
     table.addEventListener("click", currentStep);
     playerSequence = {
-        x: "",
-        o: "",
+        x: [],
+        o: [],
     };
-    player = "o";
+    player = "x";
     stepCount = 0;
     message.innerText = "Ходит игрок " + player;
+
     for (let i = 0; i < ceil.length; i++) {
         ceil[i].innerText = "";
     }
 });
 
 function isWin(sequence) {
-    return winCombinations.some(function(elem) {
-        return elem === sequence;
+    if (sequence.length < 3) return false;
+
+    return winCombinations.some(function(element) {
+        return element.every(function (el) {
+            return sequence.includes(el);
+        })
     });
 }
